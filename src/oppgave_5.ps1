d67e00f074864046f919fa50f93e1bf7ff544ffa
runemarthinsen <rune.marthinsen@nav.no>
2022-02-07 15:09:20 +0100

    Oppgave_5 - summere kortstokk

---
+++ src/oppgave_5.ps1
@@ -0,0 +1,34 @@
+param ($url_input)
+#url å bruke: "http://nav-deckofcards.herokuapp.com/shuffle"
+$ErrorActionPreference = 'Stop'
+
+$kortstokk = Invoke-WebRequest -uri $url_input
+$kortstokk = $kortstokk.content | ConvertFrom-Json
+
+#legge foreach i funkjson - returnerer streng - siste karakter vha substring
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ""
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + $($kort.value) + ","
+    }
+    return $streng = $streng.Substring(0,$streng.Length-1)
+}
+$poengKortstokk = 0
+foreach ($kort in $kortstokk) {
+    if ($kort.value -ceq 'J' -or $kort.value -ceq 'Q' -or $kort.value -ceq 'K')  {
+        $poengKortstokk = $poengKortstokk + 10
+    }
+    elseif ($kort.value -ceq 'A') {
+        $poengKortstokk = $poengKortstokk + 11
+    }
+    else {
+        $poengKortstokk = $poengKortstokk + $kort.value
+    }
+}
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+Write-Output "Poengsum: $poengkortstokk"
