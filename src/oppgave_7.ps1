58de65c59905d2d788ceba436f5c09a06d7566bb
runemarthinsen <rune.marthinsen@nav.no>
2022-02-11 10:52:15 +0100

    Oppgave 7 - sjekk hvem som vinner ev uavgjort

--- src/oppgave_7.ps1
+++ src/oppgave_7.ps1
@@ -2,10 +2,12 @@ param ($url_input)
 #url å bruke: "http://nav-deckofcards.herokuapp.com/shuffle"
 $ErrorActionPreference = 'Stop'
 
-$kortstokk = Invoke-WebRequest -uri $url_input
+#satt input fra url til fast - kan også benytte $url_input for å kjøre script med input parameter
+$kortstokk = Invoke-WebRequest -uri "http://nav-deckofcards.herokuapp.com/shuffle"
 $kortstokk = $kortstokk.content | ConvertFrom-Json
+$blackjack = 21
 
-#legge foreach i funkjson - returnerer streng - siste karakter vha substring
+#legge foreach i funkjson - returnerer streng - siste karakter fjernes (,) vha substring
 function kortstokkTilStreng {
     [OutputType([string])]
     param (
@@ -18,27 +20,97 @@ function kortstokkTilStreng {
     }
     return $streng = $streng.Substring(0,$streng.Length-1)
 }
+#summere kortstokk funksjon - vha switch
+function sumPoengKortstokk {
+    [OutputType([int])]
+    param (
+        [object[]]
+        $kortstokk
+    )
 
-$poengKortstokk = 0
-foreach ($kort in $kortstokk) {
-    if ($kort.value -ceq 'J' -or $kort.value -ceq 'Q' -or $kort.value -ceq 'K')  {
-        $poengKortstokk = $poengKortstokk + 10
+    $poengKortstokk = 0
+
+    foreach ($kort in $kortstokk) {
+        $poengKortstokk += switch ($kort.value) {
+            { $_ -cin @('J', 'Q' , 'K') } { 10 }
+            'A' { 11 }
+            default { $kort.value }
+        }
     }
-    elseif ($kort.value -ceq 'A') {
-        $poengKortstokk = $poengKortstokk + 11
-    }
-    else {
-        $poengKortstokk = $poengKortstokk + $kort.value
-    }
+    return $poengKortstokk
 }
-Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+#funksjon definisjon med fore-each løkke
+#function sumpoengkortstokk {
+#    [OutputType([int])]
+#    param (
+#        [object[]]
+#        $kortstokk
+#    )
+#    $poengKortstokk = 0
+#    foreach ($kort in $kortstokk) {
+#        if ($kort.value -ceq 'J' -or $kort.value -ceq 'Q' -or $kort.value -ceq 'K')  {
+#            $poengKortstokk = $poengKortstokk + 10
+#        }
+#        elseif ($kort.value -ceq 'A') {
+#            $poengKortstokk = $poengKortstokk + 11
+#        }
+#        else {
+#            $poengKortstokk = $poengKortstokk + $kort.value
+#        }  
+#    }
+#    return $poengkortstokk
+#}
 
-$meg = $(kortstokktilstreng -kortstokk $kortstokk[0..1])
-$kortstokk = $kortstokk[2..$kortstokk.count]
-$magnus =$(kortstokktilstreng -kortstokk $kortstokk[0..1])
-$kortstokk = $kortstokk[2..$kortstokk.count]
+# eks på for-each løkke beregne verdien i kortstokken
+#$poengKortstokk = 0
+#foreach ($kort in $kortstokk) {
+#    if ($kort.value -ceq 'J' -or $kort.value -ceq 'Q' -or $kort.value -ceq 'K')  {
+#        $poengKortstokk = $poengKortstokk + 10
+#    }
+#    elseif ($kort.value -ceq 'A') {
+#        $poengKortstokk = $poengKortstokk + 11
+#    }
+#    else {
+#        $poengKortstokk = $poengKortstokk + $kort.value
+#    }
+#}
+#Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
 
-Write-Output "Poengsum: $poengkortstokk"
-Write-Output "Mine kort: $meg"
-Write-Output "Magnus sine kort: $magnus"
-Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+#mine kort - justerer kortstokken etterpå
+$meg = $kortstokk[0..1]
+$kortstokk = $kortstokk[2..$kortstokk.Count]
+#magnus sine kort - justerer kortstokken etterpå
+$magnus = $kortstokk[0..1]
+$kortstokk = $kortstokk[2..$kortstokk.Count]
+#lager variabel med poengsum med funksjon og input min/magnus sine kort
+$minPoengsum = sumPoengKortstokk -kortstokk $meg
+$magnusPoengsum = sumPoengKortstokk -kortstokk $magnus
+
+#skriver ut resultat
+Write-Output "Mine kort:  $(kortstokkTilStreng -kortstokk $meg) "
+Write-Output "Min poengsum: $minpoengsum"
+Write-Output "Magnus sine kort:  $(kortstokkTilStreng -kortstokk $magnus) "
+Write-Output "Magnus sin poengsum: $magnuspoengsum"
+
+#elseif for å sjekke hvem som vinner - evt 'uavgjort' hvis begge får 21 eller lik poengsum
+if (($magnusPoengsum -eq $blackjack) -and ($minPoengsum -eq $blackjack)) {
+    $vinner = "Uavgjort"
+}
+elseif ($minPoengsum -eq $blackjack) {
+    $vinner = "meg"
+}
+elseif ($magnusPoengsum -eq $blackjack ) {
+    $vinner = "Magnus"
+}
+elseif ($magnusPoengsum -eq $minPoengsum ) {
+    $vinner = "Uavgjort"
+}
+elseif ($magnusPoengsum -ge $minPoengsum ) {
+    $vinner = "Magnus"
+}
+elseif ($minpoengsum -ge $magnusPoengsum  ) {
+    $vinner = "Meg"
+}
+
+#skriver ut vinner
+Write-Output "Vinner: $vinner"
